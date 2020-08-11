3fa943a7b5149b3a18d9c659bc2f7242facba68f
mhafet17 <do.crazy192@gmail.com>
2020-08-11 23:27:59 +0700

    create component emoji

---
+++ src/EmojiDisplay.svelte
@@ -0,0 +1,15 @@
+<script>
+  let currentEmoji = "😊";
+  setTimeout(function () {
+    currentEmoji = "😢";
+  }, 3000);
+</script>
+
+<style>
+  div {
+    font-size: 3em;
+    margin-bottom: 0.5em;
+  }
+</style>
+
+<div>{currentEmoji}</div>
