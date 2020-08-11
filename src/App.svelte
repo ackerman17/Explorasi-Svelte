8d8dce058269435101459d7ac462cdd80b39ecdc
mhafet17 <do.crazy192@gmail.com>
2020-08-11 23:27:30 +0700

    design app

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,27 @@
 <script>
-	export let name;
+  import EmojiDisplay from "./EmojiDisplay.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  button {
+    background-color: aqua;
+    padding: 0.75em;
+    border-radius: 0.25em;
+    border: 2px solid #000;
+    box-shadow: 0.4rem 0.4rem 0 #222;
+  }
+  button:hover {
+    box-shadow: 0.25rem 0.25rem 0 #222;
+    transition: all 0.4s ease 0s;
+    background-color: aquamarine;
+  }
+  div {
+    margin: 2em;
+  }
 </style>
+
+<div>
+  <h1>Randomize Emoji</h1>
+  <EmojiDisplay />
+  <button>✔ Randomize</button>
+</div>
